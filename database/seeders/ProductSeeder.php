8560a4659553375337131352dbfe0c0a8701478c
Rayen Bourguiba <rayen.bourguiba@esprit.tn>
2023-10-01 23:18:59 +0100

    Seeders

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Product::create([
+            'name' => 'Product 1',
+            'description' => 'This is a product description.',
+            'quantity' => 10,
+            'price' => 29.99,
+            'image' => 'sample_product1.jpg',
+            'user_id' => 1,
+        ]);
+
+        Product::create([
+            'name' => 'Product 2',
+            'description' => 'Another product description.',
+            'quantity' => 5,
+            'price' => 19.99,
+            'image' => 'product2.jpg',
+            'user_id' => 1,
+        ]);
+    }
+}
