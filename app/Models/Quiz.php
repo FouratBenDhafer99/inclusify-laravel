b60bf6d5ea0ee6cb737879521a5a4926139d9d1a
Fourat Ben Dhafer <fouratbendhafer99@gmail.com>
2023-10-17 03:36:59 +0100

    Skill and Quiz almost complete

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -2,17 +2,23 @@
 
 namespace App\Models;
 
+use App\Models\ORM\Extension\BlamableAwareInterface;
+use App\Models\ORM\Extension\Traits\BlamableTableTrait;
+use App\Models\ORM\Extension\Traits\ModelCreatingUpdatingTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Quiz extends Model
+class Quiz extends Model implements BlamableAwareInterface
 {
     use HasFactory;
+    use BlamableTableTrait;
+    use ModelCreatingUpdatingTrait;
 
 
     protected $fillable = [
         'score',
         'isSuccessful',
+        'isFinished'
     ];
 
     public function skill()
