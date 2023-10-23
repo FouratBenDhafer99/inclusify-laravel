0783f69a8ecb9de02b9df6932adab770a2066140
Youssef ALMIA <almia.youssef2@gmail.com>
2023-10-23 15:18:59 +0100

    rebase

---
+++ app/Models/Post.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'description',
+        'images',
+        'postTypeEnum',
+        'user_id',
+    ];
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reacts()
+    {
+        return $this->hasMany(PostReact::class);
+    }
+
+
+}
